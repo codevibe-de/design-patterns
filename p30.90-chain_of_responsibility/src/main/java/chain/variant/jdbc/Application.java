08fb8a18d0c8d32192795a2c920588a9f89778c4
Thomas Auinger <thomas@auinger.de>
2023-10-11 16:25:31 +0200

    fixed jdbc url

--- p30.90-chain_of_responsibility/src/main/java/chain/variant/jdbc/Application.java
+++ p30.90-chain_of_responsibility/src/main/java/chain/variant/jdbc/Application.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        Class.forName("simple.SimpleDriver");
+        Class.forName("chain.variant.jdbc.SimpleDriver");
         final Connection con = DriverManager.getConnection("jdbc:simple//xyz");
         System.out.println(con.getClass().getName());
     }
